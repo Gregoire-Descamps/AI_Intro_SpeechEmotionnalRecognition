70ec44e3f0c2b786147b9eb12488683e1e169a4e
Gregoire Descamps <gregoire.descamps@lacatholille.fr>
2024-12-27 17:46:58 +0100

    First model implementation, very poor accuracy (46%)

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ import librosa
 import librosa.display
 import IPython.display as ipd
 
+#Sklearn
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
 
 from itertools import cycle
 
@@ -19,12 +22,12 @@ sns.set_theme(style='white', palette = None)
 color_pal = plt.rcParams['axes.prop_cycle'].by_key()["color"]
 
 audio_files = glob.glob('CREMA-D/AudioWAV/*.wav')
-emotions = {"ANG" :"anger",
-            "DIS":"disgust",
-            "FEA":"fear",
-            "NEU":"neutral",
-            "HAP":"happy",
-            "SAD":"sad"}
+emotions = {"ANG" :0,
+            "DIS":1,
+            "FEA":2,
+            "HAP":3,
+            "NEU":4,
+            "SAD":5}
 
 emotions_level = {"XX":0,
                   "LO": 1,
@@ -68,4 +71,31 @@ def LoadData(test_size =0.2):
 
     return train_test_split(np.array(x), y, test_size= test_size, random_state = 9)
 
-print(LoadData())
+x_train, x_test, y_train, y_test = LoadData()
+
+
+#Shape of train and test set and Number of features extracted
+print((x_train.shape[0], x_test.shape[0]))
+print(f'Features extracted: {x_train.shape[1]}')
+
+model_basic = MLPClassifier(hidden_layer_sizes=(150,100,100,), activation="relu",
+                            solver="adam", alpha=0.0001,
+                            batch_size=250, learning_rate="constant",
+                            learning_rate_init=0.001, max_iter=300,
+                            shuffle=True, random_state=42,
+                            tol=1e-5, verbose=True,
+                            momentum=0.9, nesterovs_momentum=True,
+                            beta_1=0.9, beta_2=0.999,
+                            epsilon=1e-08, n_iter_no_change=50,
+                            )
+
+
+model_basic.fit(x_train, y_train)
+
+# Predict for the test set
+y_pred = model_basic.predict(x_test)
+
+# Calculate Accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy: {:.2f}%".format(accuracy * 100))
+
