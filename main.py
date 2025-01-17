27357d6223a6b8dc23d1534f3c7bcaf5ccca5b56
Gregoire-Descamps <gregoire.descamps@lacatholille.fr>
2025-01-17 18:07:39 +0100

    Patched Code (post-project submission)
    Signed-off-by: Gregoire-Descamps <gregoire.descamps@lacatholille.fr>

--- main.py
+++ main.py
@@ -2,22 +2,17 @@ import os
 import random
 import numpy as np
 import matplotlib.pylab as plt
-import seaborn as sns
 import soundfile
 import glob
 import librosa
-import librosa.display
-import winsound
 
 #Sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 
-from itertools import cycle
 
-sns.set_theme(style='white', palette = None)
-color_pal = plt.rcParams['axes.prop_cycle'].by_key()["color"]
+
 
 audio_files = glob.glob('CREMA-D/AudioWAV/*.wav')
 emotions = {"ANG": "anger",
@@ -66,7 +61,7 @@ def extract_feature(file_name, mfcc, chroma, mel, augmentation = None):
 
 
 
-def LoadData(test_size =0.2):
+def loadData(test_size =0.2, type = None):
     x, y = [], []
     for file in glob.glob('CREMA-D/AudioWAV/*.wav'):
         file_name = os.path.basename(file)
@@ -75,79 +70,76 @@ def LoadData(test_size =0.2):
         if emotion not in emotions.values():
             continue
         # match case to create a training dataset with  an addition of 20% of random augmented data
-        if True and random.randint(0,4)<1:
-            match random.randint(1,3):
-                case 1:
-                    x.append(extract_feature(file, mfcc=True, chroma=True, mel=True, augmentation="noise"))
-                    y.append(emotion)
-                case 2:
-                    x.append(extract_feature(file, mfcc=True, chroma=True, mel=True, augmentation="pitch"))
-                    y.append(emotion)
-                case 3:
-                    x.append(extract_feature(file, mfcc=True, chroma=True, mel=True, augmentation="stretch"))
-                    y.append(emotion)
-                case _:
-                    pass
+        if type == "mixed":
+            x.append( extract_feature(file, mfcc=True, chroma=True, mel=True))
+            y.append(emotion)
 
-        feature = extract_feature(file, mfcc=True, chroma=True, mel=True)
-        x.append(feature)
-        y.append(emotion)
+            if random.randint(0,4)<1 :
+                match random.randint(1,3):
+                    case 1:
+                        x.append(extract_feature(file, mfcc=True, chroma=True, mel=True, augmentation="noise"))
+                        y.append(emotion)
+                    case 2:
+                        x.append(extract_feature(file, mfcc=True, chroma=True, mel=True, augmentation="pitch"))
+                        y.append(emotion)
+                    case 3:
+                        x.append(extract_feature(file, mfcc=True, chroma=True, mel=True, augmentation="stretch"))
+                        y.append(emotion)
+                    case _:
+                        pass
 
-    return train_test_split(np.array(x), y, test_size= test_size, random_state = 9)
+        elif type == "noisy" :
+            x.append(extract_feature(file, mfcc=True, chroma=True, mel=True, augmentation="noise"))
+            y.append(emotion)
 
-# x_train, x_test, y_train, y_test = LoadData()
-#
-# # Lts do data normalization
-# mean = np.mean(x_train, axis=0)
-# std = np.std(x_train, axis=0)
-#
-# x_train = (x_train - mean)/std
-# x_test = (x_test - mean)/std
-# np.save("x_train_mixed",x_train)
-# np.save("y_train_mixed",y_train)
-# np.save("x_test_mixed",x_test)
-# np.save("y_test_mixed",y_test)
-#
-# winsound.Beep(1000,500)
+        elif type == "pitched" :
+            x.append(extract_feature(file, mfcc=True, chroma=True, mel=True, augmentation="pitch"))
+            y.append(emotion)
+
+        elif type == "stretched" :
+            x.append(extract_feature(file, mfcc=True, chroma=True, mel=True, augmentation="stretch"))
+            y.append(emotion)
+
+        elif type == None:
+            feature = extract_feature(file, mfcc=True, chroma=True, mel=True)
+            x.append(feature)
+            y.append(emotion)
+
+        else:
+            raise(f'Argument \"{type}\" for dataset type not recognized.')
+
+    return train_test_split(np.array(x), y, test_size=test_size, random_state=9)
+
+
+def datasetGeneration():
+
+    datasetTypes = [None, "noisy", "pitched", "stretched", "mixed"]
+
+    for type in datasetTypes:
+        x_train, x_test, y_train, y_test = loadData(type=type)
+
+        # data normalization
+        mean = np.mean(x_train, axis=0)
+        std = np.std(x_train, axis=0)
+        x_train = (x_train - mean)/std
+        x_test = (x_test - mean)/std
+
+        if type != None:
+            datasetType = "_" + str(type)
+        else:
+            datasetType = ""
+
+        np.save("x_train" + datasetType,x_train)
+        np.save("y_train" + datasetType,y_train)
+        np.save("x_test" + datasetType,x_test)
+        np.save("y_test" + datasetType,y_test)
+
 
 def loadFromBinary(x_train, y_train, x_test, y_test):
     return np.load(x_train), np.load(x_test), np.load(y_train), np.load(y_test)
 
-x_train, x_test, y_train, y_test = loadFromBinary("x_train_mixed.npy", "y_train_mixed.npy",
-                                                  "x_test_mixed.npy", "y_test_mixed.npy")
 
-
-
-#Shape of train and test set and Number of features extracted
-print((x_train.shape[0], x_test.shape[0]))
-print(f'Features extracted: {x_train.shape[1]}')
-
-
-model_basic = MLPClassifier(hidden_layer_sizes=(200,100,50,), activation="relu",
-                            solver="adam", alpha=0.0001,
-                            batch_size=300, learning_rate="constant",
-                            learning_rate_init=0.001, max_iter=250,
-                            shuffle=True, random_state=42,
-                            tol=1e-4, verbose=True, early_stopping= False,
-                            momentum=0.9, nesterovs_momentum=True,
-                            beta_1=0.9, beta_2=0.999,
-                            epsilon=1e-08, n_iter_no_change=50,
-                            )
-
-
-model_test = MLPClassifier(hidden_layer_sizes=(200,200,200,200), activation="relu",
-                            solver="adam", alpha=0.0001,
-                            batch_size=300, learning_rate="constant",
-                            learning_rate_init=0.001, max_iter=1000,
-                            shuffle=True, random_state=42,
-                            tol=1e-4, verbose=False,
-                            momentum=0.9, nesterovs_momentum=True,
-                            beta_1=0.9, beta_2=0.999,
-                            epsilon=1e-08, n_iter_no_change=50,
-                            )
-
-
-# Fit model and plot accuracy on train and tes data over epoch
+# Fit model and plot accuracy on train and test data over 100 epochs
 def PartialFit(model, n_epoch =100, minibatch_size = None, plot = True):
     scores_train = []
     scores_test = []
@@ -202,13 +194,14 @@ def plotLoss(model):
     plt.plot(model.loss_curve_)
     if model.early_stopping:
         plt.plot(model.validation_scores_)
-    plt.title(f"Loss over mini epochs (Test {model.solver}, {model.hidden_layer_sizes} , batch {model.batch_size},\n"
+    plt.title(f"Loss over mini batch (Test {model.solver}, {model.hidden_layer_sizes} , batch {model.batch_size},\n"
               f"LR {model.learning_rate}({model.learning_rate_init})", fontsize=14)
-    plt.xlabel('Mini Epochs')
+    plt.xlabel('Mini Batch')
     plt.legend(loc='upper right')
     plt.show()
 
-# allow to provide model parameter to test and compare, return a dict of average accuracy results
+# Provide multiple model parameter lists to test and compare in order to find optimal parameters,
+# return a dict of average accuracy results over parameter sets.
 def optimalTuning(n_rounds = 10, solvers = ["adam"], hidden_layers_shapes = [(200,100,50)], batch_sizes = [300], learning_rates = ["constant"], learning_rate_inits = [0.0001], tols = [1e-4], n_iter_no_changes = [50]):
     results = {}
     for solver in solvers:
@@ -252,6 +245,45 @@ def directFit(model):
     print(predictScore(fitted_model))
     plotLoss(fitted_model)
 
+
+
+# datasetGeneration()
+
+
+x_train, x_test, y_train, y_test = loadFromBinary("x_train_mixed.npy", "y_train_mixed.npy",
+                                                  "x_test_mixed.npy", "y_test_mixed.npy")
+
+
+
+#Shape of train and test set and Number of features extracted
+print((x_train.shape[0], x_test.shape[0]))
+print(f'Features extracted: {x_train.shape[1]}')
+
+
+model_basic = MLPClassifier(hidden_layer_sizes=(200,100,50,), activation="relu",
+                            solver="adam", alpha=0.0001,
+                            batch_size=300, learning_rate="constant",
+                            learning_rate_init=0.001, max_iter=250,
+                            shuffle=True, random_state=42,
+                            tol=1e-4, verbose=True, early_stopping= False,
+                            momentum=0.9, nesterovs_momentum=True,
+                            beta_1=0.9, beta_2=0.999,
+                            epsilon=1e-08, n_iter_no_change=50,
+                            )
+
+
+model_test = MLPClassifier(hidden_layer_sizes=(200,200,200,200), activation="relu",
+                            solver="adam", alpha=0.0001,
+                            batch_size=300, learning_rate="constant",
+                            learning_rate_init=0.001, max_iter=1000,
+                            shuffle=True, random_state=42,
+                            tol=1e-4, verbose=False,
+                            momentum=0.9, nesterovs_momentum=True,
+                            beta_1=0.9, beta_2=0.999,
+                            epsilon=1e-08, n_iter_no_change=50,
+                            )
+
+
 #
 model = model_test
 # accuracies = []
@@ -262,5 +294,5 @@ model = model_test
 
 # directFit(model)
 
+# plot training loss on train dataset
 plotLoss(PartialFit(model))
-winsound.Beep(1000,500)
